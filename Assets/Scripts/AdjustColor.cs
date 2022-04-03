f0c50b5fdd895d3b1e4d25c498cf23ee4cc6fa63
penta-dev <jinjinri10@gmail.com>
2022-04-04 01:21:58 +0900

    1.0.0

--- Assets/Scripts/AdjustColor.cs
+++ Assets/Scripts/AdjustColor.cs
@@ -17,6 +17,7 @@ public class AdjustColor : MonoBehaviour
         _index = 0;
         _timeLeft = _duration;
         _renderer = gameObject.GetComponent<Renderer>();
+        _renderer.material.EnableKeyword("_EMISSION");
     }
 
     
@@ -30,18 +31,64 @@ public class AdjustColor : MonoBehaviour
 
             color = _index % 2 == 0 ? _firstColor : _secondColor;
             _renderer.material.SetColor("_Color", color);
-            _renderer.material.SetColor("_EmissionColor", color *2);
+            _renderer.material.SetColor("_EmissionColor", color);
+            SetEmissionIntensity(_renderer.material, 2);
 
         }
         else
         {
-            color = Color.Lerp(_renderer.material.color, _index % 2 == 0 ? _secondColor : _firstColor, Time.deltaTime / _timeLeft);
+            Color from, to;
+            if (_index % 2 == 0)
+            {// first->second
+                from = _firstColor;
+                to = _secondColor;
+            }
+            else
+            {//second->first
+                from = _secondColor;
+                to = _firstColor;
+            }
+
+            if (_timeLeft < _duration / 3)
+            {// blue change
+                float ratio = 1 - _timeLeft / (_duration / 3); // 0 ~ 1
+                float b = from.b + (to.b - from.b) * ratio;
+                color = new Color(to.r, to.g, b);
+                Debug.Log("B change: " + b + color);
+            }
+            else if (_timeLeft < _duration * 2 / 3)
+            {// green change
+                float ratio = 1 - (_timeLeft - _duration / 3) / (_duration / 3); // 0 ~ 1
+                float g = from.g + (to.g - from.g) * ratio;
+                color = new Color(to.r, g, from.b);
+                Debug.Log("G change: " + g + color);
+            }
+            else
+            {// red change
+                float ratio = 1 - (_timeLeft - _duration * 2 / 3) / (_duration / 3); // 0 ~ 1
+                float r = from.r + (to.r - from.r) * ratio;
+                color = new Color(r, from.g, from.b);
+                Debug.Log("R change: " + r + color);
+            }
+
             _renderer.material.SetColor("_Color", color);
-            _renderer.material.SetColor("_EmissionColor", color *2);
+            _renderer.material.SetColor("_EmissionColor", color);
+            SetEmissionIntensity(_renderer.material, 1);
 
             _timeLeft -= Time.deltaTime;
         }
+    }
+    //https://forum.unity.com/threads/setting-material-emission-intensity-in-script-to-match-ui-value.661624/
+    private void SetEmissionIntensity(Material mat, float intensity)
+    {
+        Color color = mat.GetColor("_Color");
 
-        Debug.Log(color);
+        // for some reason, the desired intensity value (set in the UI slider) needs to be modified slightly for proper internal consumption
+        float adjustedIntensity = intensity - (0.4169F);
+
+        // redefine the color with intensity factored in - this should result in the UI slider matching the desired value
+        color *= Mathf.Pow(2.0F, adjustedIntensity);
+        mat.SetColor("_EmissionColor", color);
+        //Debug.Log("<b>Set custom emission intensity of " + intensity + " (" + adjustedIntensity + " internally) on Material: </b>" + mat);
     }
 }
